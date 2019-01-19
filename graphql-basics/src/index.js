77b0c68fed98e53989168de279a4461343f3dcc7
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2019-01-18 18:37:17 -0600

    Adding filters o array queries

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,11 +1,61 @@
 import { GraphQLServer } from 'graphql-yoga'
 
+// demo user data
+
+const users = [
+    {
+        id: '1',
+        name: 'Juan',
+        email: 'Juan@fake.com',
+        age: 20
+    },
+    {
+        id: '2',
+        name: 'Sarah',
+        email: 'Sarah@fake.com',
+        age: 25
+    },
+    {
+        id: '3',
+        name: 'Mike',
+        email: 'Mike@fake.com',
+        age: 18
+    }
+]
+
+const posts = [
+    {
+        id:'1',
+        title: 'first title',
+        body: 'Just a initial post for testing',
+        published: true
+    },
+    {
+        id:'2',
+        title: 'second title',
+        body: 'scond post used for demo',
+        published: false
+    },
+    {
+        id:'3',
+        title: 'a post',
+        body: 'Amlo is a looser',
+        published: true
+    },
+    {
+        id:'4',
+        title: 'draft',
+        body: 'this post was never published',
+        published: false
+    }
+]
+
+
 // type definitions (Schema)
 const typeDefs = `
     type Query {
-        add(numbers: [Float!]!): Float!
-        greeting(name: String): String!
-        grades: [Int!]!
+        users(query: String): [User!]!
+        posts(query: Boolean): [Post!]!
         me: User!
         post: Post!
     }
@@ -22,27 +72,27 @@ const typeDefs = `
         id: ID!
         title: String!
         body: String!
-        published: String!
+        published: Boolean!
     }
 `
 
 // Resolvers
 const resolvers = {
     Query: {
-        add(parent, args){
-            if(args.numbers.length == 0){
-                return 0
+        users(parent, args, ctx, info){
+            if (!args.query){
+                return users
             }
 
-            return args.numbers.reduce((acum, val) =>  acum + val)
-            
+            return users.filter(elem => 
+                    elem.name.toLowerCase().includes(args.query.toLowerCase()))
         },
-        greeting(parent, args, ctx, info){
-            console.log('args', args);
-            return `Hello ${args.name}`;
-        },
-        grades(parent, args){
-            return [99, 15, 80, 46, 35]
+        posts(parent, args, ctx, info){
+            if(!args.query){
+                return posts
+            }
+
+            return posts.filter(elem => elem.published == args.query)
         },
         me () {
            return {
@@ -56,7 +106,7 @@ const resolvers = {
                id: 'cba321',
                title: 'test post',
                body: 'some a cool body!',
-               published: 'some one'
+               published: true
            }
         }
     }
