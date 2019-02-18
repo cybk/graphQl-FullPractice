d2dc15f3ecb193e5efd7f4e06e66a1cb7de2322b
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2019-02-18 11:40:45 -0600

    Added bindings for user mutations

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -1,46 +1,29 @@
 import uuidv4  from 'uuid/v4'
 
 const Mutation= {
-    createUser (parent, args, {db}, info){
-        const emailTaken = db.users.some( elem => elem.email === args.data.email);
+    async createUser (parent, args, {prisma}, info){
+        const userExist = await prisma.exists.User({email: args.data.email});
 
-        if (emailTaken){
+        if (userExist){
             throw new Error('Email already taken.');
         }
 
-        const user = {
-            id: uuidv4(),
-            ...args.data
-
-        }
-
-        db.users.push(user);
-
-        return user;
-
+        return prisma.mutation.createUser({
+            data: args.data
+        }, info);
     },
-    deleteUser (parent, args, {db}, info) {
-        const userIndex = db.users.findIndex(elem => elem.id === args.user);
+    async deleteUser (parent, args, {prisma}, info) {
+        const userExist = await prisma.exists.User({id: args.user});
 
-        if(userIndex < 0){
+        if(!userExist){
             throw new Error('User does not exist.');
         }
 
-        const deletedUsers = db.users.splice(userIndex, 1);
-
-        db.posts = db.posts.filter(elem => {
-           const match = elem.author === args.user;
-           
-           if (match){
-               comments = db.comments.filter((comment) => comment.post !== elem.id);
-           }
-
-           return !match
-        });
-
-        db.comments = db.comments.filter (comment => comment.author !== args.user);
-
-        return deletedUsers[0];
+        return prisma.mutation.deleteUser({
+            where: {
+                id: args.user
+            }
+        }, info);
     },
     updateUser(parent, {id, data}, {db}, info){
 
