604d371bdae754edcb49ecfee7de56b105853b36
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2019-02-21 17:06:50 -0600

    Adding prisma for the comment methods

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -12,6 +12,7 @@ const Mutation= {
             data: args.data
         }, info);
     },
+
     async deleteUser (parent, args, {prisma}, info) {
         const userExist = await prisma.exists.User({id: args.user});
 
@@ -25,6 +26,7 @@ const Mutation= {
             }
         }, info);
     },
+
     async updateUser(parent, args, {prisma}, info){
         return prisma.mutation.updateUser({
             where: {
@@ -33,6 +35,7 @@ const Mutation= {
             data: args.data
         }, info);
     },
+
     async createPost (parent, args, {prisma}, info){
         return prisma.mutation.createPost({ 
             data: {
@@ -47,142 +50,69 @@ const Mutation= {
             }
         }, info);
     },
-    deletePost (parent, args, {db}, info) {
-        const postIndex = db.posts.findIndex(elem => elem.id === args.post);
 
-        if (postIndex < 0){
+    async deletePost (parent, args, {prisma}, info) {
+        const postExist = await prisma.exists.Post({id: args.post})
+
+        if (!postExist){
             throw new Error ("Post does not exist.");
         }
 
-        const [post] = db.posts.splice(postIndex, 1);
-
-        db.comments = db.comments.filter(comment => comment.post !== args.post);
-
-        if (post.published){
-            pubsub.publish('post', {
-                post:{
-                    mutation: 'DELETED',
-                    data: post
-                }
-            });
-        }
-
-        return deletedPost[0];
+        return prisma.mutation.deletePost({
+            where: {
+                id: args.post
+            }
+        }, info);
     },
-    updatePost (parent, args, {db, pubsub}, info){
-        const  {id, data} = args;
-        const post = db.posts.find(elem => elem.id == id);
-        const originalPost = {...post};
 
-        if (!post){
-            throw new Error('Post does not exist.');
-        }
+    async updatePost (parent, args, { prisma }, info){
+        return prisma.mutation.updatePost({
+            where: {
+                id: args.id
+            },
+            data: args.data
+        }, info);
+    },
 
-        if (typeof data.title === 'string'){
-            post.title = data.title;
-        }
-
-        if (typeof data.body === 'string'){
-            post.body = data.body;
-        }
-
-        if (typeof data.published === 'boolean'){
-            post.published = data.published;
-
-            if (originalPost.published && !post.published){
-                pubsub.publish('post', {
-                    post: {
-                        mutation: 'DELETED',
-                        data: originalPost
+    async createComment (parent, args, {prisma}, info){
+        return prisma.mutation.createComment({
+            data: {
+                text: args.data.text,
+                author: {
+                    connect: {
+                        id: args.data.author
                     }
-                });
-            }else if (!originalPost.published && post.published) {
-                pubsub.publish('post', {
-                    post: {
-                        mutation: 'CREATED',
-                        data: post
-                    }
-                });
-
-            } 
-        } else if(post.published ){
-            pubsub.publish('post', {
+                },
                 post: {
-                    mutation: 'UPDATED',
-                    data: post
+                    connect: {
+                        id: args.data.post
+                    }
                 }
-            });
-        }
-
-        return post;
-    },
-    createComment (parent, args, {db, pubsub}, info){
-        const userExists = db.users.some(elem => elem.id === args.data.author);
-        if (!userExists){
-            throw new Error('User does not exists.');
-        }
-
-        const postExist = db.posts.some(elem => elem.id === args.data.post && elem.published);
-        if (!postExist){
-            throw new Error('Post does not exists or it has not been published.');
-        }
-
-        const comment = {
-            id: uuidv4(),
-            ...args.data
-        }
-
-        
-        db.comments.push(comment);
-        
-        pubsub.publish(`comment ${args.data.post}`, {
-          comment: {
-              mutation: 'CREATED',
-              data: comment
-          }  
-        });
-
-        return comment;
-    },
-    deleteComment (parent, args, {db, pubsub}, info){
-        const commentIndex = db.comments.findIndex(elem => elem.id === args.comment);
-
-        if (commentIndex < 0){
-            throw new Error("Comment does not exist.");
-        }
-
-        const [deletedComment] = db.comments.splice(commentIndex, 1);
-
-        pubsub.publish(`comment ${deletedComment.post}`, {
-            comment: {
-                mutation: 'DELETED',
-                data: deletedComment
             }
-        });
-
-        return deletedComment;
-
+        }, info);
     },
-    updateComment (parent, args, {db, pubsub}, info){
-        const { id, text} = args;
-        const comment = db.comments.find(elem => elem.id === id);
 
-        if (!comment){
-            throw new Error('Comment does not exist.');
+    async deleteComment (parent, args, {prisma}, info){
+        const  commentExist = await prisma.exists.Comment({id: args.comment});
+
+        if (!commentExist){
+            throw new Error("Comment does not exist");
         }
 
-        if (typeof text === 'string'){
-            comment.text = text;
-        }
-
-        pubsub.publish(`comment ${comment.post}`, {
-            comment: {
-                mutation: 'UPDATED',
-                data: comment
+        return prisma.mutation.deleteComment({
+            where: {
+                id: args.comment
             }
-        })
+        }, info);
+    },
 
-        return comment;
+    async updateComment (parent, args, {prisma}, info){
+        return prisma.mutation.updateComment({
+            where: {
+                id: args.id
+            },
+            data: args.data
+        }, info);
     }
 }
 
